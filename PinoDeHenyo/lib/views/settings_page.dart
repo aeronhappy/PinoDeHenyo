673fd618b2715718f893d537934e715a4db13bee
aeronhappy <maligaya.ads@gmail.com>
2023-11-13 21:24:26 +0800

    done

--- PinoDeHenyo/lib/views/settings_page.dart
+++ PinoDeHenyo/lib/views/settings_page.dart
@@ -48,185 +48,203 @@ class _SettingsPageState extends State<SettingsPage> {
             fit: BoxFit.fitHeight,
           ),
         ),
-        Material(
-          color: Colors.transparent,
-          child: Container(
-            padding: EdgeInsets.fromLTRB(
-                20, MediaQuery.of(context).size.height * .15, 20, 0),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Column(
-                  children: [
-                    Text('Music Volume', style: largeTitleWhite(true)),
-                    SizedBox(height: 5),
-                    SliderTheme(
-                      data: SliderTheme.of(context).copyWith(
-                          inactiveTrackColor: Colors.black38,
-                          activeTrackColor: Colors.red,
-                          thumbColor: red,
-                          thumbShape:
-                              RoundSliderThumbShape(enabledThumbRadius: 13),
-                          overlayShape:
-                              RoundSliderOverlayShape(overlayRadius: 10),
-                          trackHeight: 13,
-                          trackShape: RoundedRectSliderTrackShape()),
-                      child: Slider(
-                        value: musicValue,
-                        max: 1,
-                        onChangeEnd: (value) {
-                          playMusic();
-                        },
-                        onChanged: (value) async {
-                          setState(() {
-                            musicValue = value;
-                          });
-                          var sharedPref =
-                              await SharedPreferences.getInstance();
-                          await sharedPref.setDouble('MusicVolume', value);
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 50),
-                Column(
-                  children: [
-                    Text('Sound Effects', style: largeTitleWhite(true)),
-                    SizedBox(height: 5),
-                    SliderTheme(
-                      data: SliderTheme.of(context).copyWith(
-                          inactiveTrackColor: Colors.black38,
-                          activeTrackColor: Colors.red,
-                          thumbColor: red,
-                          thumbShape:
-                              RoundSliderThumbShape(enabledThumbRadius: 13),
-                          overlayShape:
-                              RoundSliderOverlayShape(overlayRadius: 10),
-                          trackHeight: 13,
-                          trackShape: RoundedRectSliderTrackShape()),
-                      child: Slider(
-                        value: effectsValue,
-                        max: 1,
-                        onChangeEnd: (value) async {
-                          effectsAudioPlayer.play('bg_music/testing_bg.mp3',
-                              volume: value);
-                          var sharedPref =
-                              await SharedPreferences.getInstance();
-                          await sharedPref.setDouble('EffectsVolume', value);
-                        },
-                        onChanged: (value) {
-                          setState(() {
-                            effectsValue = value;
-                          });
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 50),
-                Column(
-                  children: [
-                    Text('Pino Volume', style: largeTitleWhite(true)),
-                    SizedBox(height: 5),
-                    SliderTheme(
-                      data: SliderTheme.of(context).copyWith(
-                          inactiveTrackColor: Colors.black38,
-                          activeTrackColor: Colors.red,
-                          thumbColor: red,
-                          thumbShape:
-                              RoundSliderThumbShape(enabledThumbRadius: 13),
-                          overlayShape:
-                              RoundSliderOverlayShape(overlayRadius: 10),
-                          trackHeight: 13,
-                          trackShape: RoundedRectSliderTrackShape()),
-                      child: Slider(
-                        value: pinoValue,
-                        onChangeEnd: (value) async {
-                          var sharedPref =
-                              await SharedPreferences.getInstance();
-                          await sharedPref.setDouble('PinoVolume', value);
-                          await flutterTts.setVolume(value);
-                          await flutterTts.speak('Ako si Pino');
-                        },
-                        onChanged: (value) {
-                          setState(() {
-                            pinoValue = value;
-                          });
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 50),
-                Column(
-                  children: [
-                    Text('Background Music', style: largeTitleWhite(true)),
-                    SizedBox(height: 10),
-                    Center(
-                      child: Container(
-                        height: 70,
-                        alignment: Alignment.center,
-                        child: ListView.builder(
-                          physics: const ClampingScrollPhysics(),
-                          shrinkWrap: true,
-                          scrollDirection: Axis.horizontal,
-                          itemCount: bgList.length,
-                          itemBuilder: (context, index) {
-                            return InkWell(
-                              borderRadius: BorderRadius.circular(100),
-                              highlightColor: Colors.red,
-                              onTap: () async {
-                                setState(() {
-                                  bgIndex = index;
-                                });
-                                var sharedPref =
-                                    await SharedPreferences.getInstance();
-                                await sharedPref.setInt('Music', index);
-                                playMusic();
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.all(3),
-                                child: Material(
-                                  borderRadius: BorderRadius.circular(100),
-                                  elevation: 5,
-                                  color: Colors.transparent,
-                                  child: Container(
-                                    width:
-                                        MediaQuery.of(context).size.width * .28,
-                                    padding: EdgeInsets.all(10),
-                                    decoration: BoxDecoration(
-                                        color: index == bgIndex
-                                            ? Colors.red
-                                            : lightPrimarybgColor,
-                                        border: Border.all(width: 2),
-                                        borderRadius:
-                                            BorderRadius.circular(100)),
-                                    child: Center(
-                                        child: Text(
-                                      "Music ${index + 1}",
-                                      style: index == bgIndex
-                                          ? bodyWhite.copyWith(
-                                              color: Colors.white)
-                                          : bodyBlack.copyWith(
-                                              color: Colors.black),
-                                    )),
-                                  ),
+        SafeArea(
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  CustomBackButton(text: 'Settings'),
+                  Expanded(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Text('Music Volume', style: largeTitleWhite(true)),
+                            SizedBox(height: 5),
+                            SliderTheme(
+                              data: SliderTheme.of(context).copyWith(
+                                  inactiveTrackColor: Colors.black38,
+                                  activeTrackColor: Colors.red,
+                                  thumbColor: red,
+                                  thumbShape: RoundSliderThumbShape(
+                                      enabledThumbRadius: 13),
+                                  overlayShape: RoundSliderOverlayShape(
+                                      overlayRadius: 10),
+                                  trackHeight: 13,
+                                  trackShape: RoundedRectSliderTrackShape()),
+                              child: Slider(
+                                value: musicValue,
+                                max: 1,
+                                onChangeEnd: (value) {
+                                  playMusic();
+                                },
+                                onChanged: (value) async {
+                                  setState(() {
+                                    musicValue = value;
+                                  });
+                                  var sharedPref =
+                                      await SharedPreferences.getInstance();
+                                  await sharedPref.setDouble(
+                                      'MusicVolume', value);
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 50),
+                        Column(
+                          children: [
+                            Text('Sound Effects', style: largeTitleWhite(true)),
+                            SizedBox(height: 5),
+                            SliderTheme(
+                              data: SliderTheme.of(context).copyWith(
+                                  inactiveTrackColor: Colors.black38,
+                                  activeTrackColor: Colors.red,
+                                  thumbColor: red,
+                                  thumbShape: RoundSliderThumbShape(
+                                      enabledThumbRadius: 13),
+                                  overlayShape: RoundSliderOverlayShape(
+                                      overlayRadius: 10),
+                                  trackHeight: 13,
+                                  trackShape: RoundedRectSliderTrackShape()),
+                              child: Slider(
+                                value: effectsValue,
+                                max: 1,
+                                onChangeEnd: (value) async {
+                                  effectsAudioPlayer.play(
+                                      'bg_music/testing_bg.mp3',
+                                      volume: value);
+                                  var sharedPref =
+                                      await SharedPreferences.getInstance();
+                                  await sharedPref.setDouble(
+                                      'EffectsVolume', value);
+                                },
+                                onChanged: (value) {
+                                  setState(() {
+                                    effectsValue = value;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 50),
+                        Column(
+                          children: [
+                            Text('Pino Volume', style: largeTitleWhite(true)),
+                            SizedBox(height: 5),
+                            SliderTheme(
+                              data: SliderTheme.of(context).copyWith(
+                                  inactiveTrackColor: Colors.black38,
+                                  activeTrackColor: Colors.red,
+                                  thumbColor: red,
+                                  thumbShape: RoundSliderThumbShape(
+                                      enabledThumbRadius: 13),
+                                  overlayShape: RoundSliderOverlayShape(
+                                      overlayRadius: 10),
+                                  trackHeight: 13,
+                                  trackShape: RoundedRectSliderTrackShape()),
+                              child: Slider(
+                                value: pinoValue,
+                                onChangeEnd: (value) async {
+                                  var sharedPref =
+                                      await SharedPreferences.getInstance();
+                                  await sharedPref.setDouble(
+                                      'PinoVolume', value);
+                                  await flutterTts.setVolume(value);
+                                  await flutterTts.speak('Ako si Pino');
+                                },
+                                onChanged: (value) {
+                                  setState(() {
+                                    pinoValue = value;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 50),
+                        Column(
+                          children: [
+                            Text('Background Music',
+                                style: largeTitleWhite(true)),
+                            SizedBox(height: 10),
+                            Center(
+                              child: Container(
+                                height: 70,
+                                alignment: Alignment.center,
+                                child: ListView.builder(
+                                  physics: const ClampingScrollPhysics(),
+                                  shrinkWrap: true,
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: bgList.length,
+                                  itemBuilder: (context, index) {
+                                    return InkWell(
+                                      borderRadius: BorderRadius.circular(100),
+                                      highlightColor: Colors.red,
+                                      onTap: () async {
+                                        setState(() {
+                                          bgIndex = index;
+                                        });
+                                        var sharedPref = await SharedPreferences
+                                            .getInstance();
+                                        await sharedPref.setInt('Music', index);
+                                        playMusic();
+                                      },
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(3),
+                                        child: Material(
+                                          borderRadius:
+                                              BorderRadius.circular(100),
+                                          elevation: 5,
+                                          color: Colors.transparent,
+                                          child: Container(
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                .28,
+                                            padding: EdgeInsets.all(10),
+                                            decoration: BoxDecoration(
+                                                color: index == bgIndex
+                                                    ? Colors.red
+                                                    : lightPrimarybgColor,
+                                                border: Border.all(width: 2),
+                                                borderRadius:
+                                                    BorderRadius.circular(100)),
+                                            child: Center(
+                                                child: Text(
+                                              "Music ${index + 1}",
+                                              style: index == bgIndex
+                                                  ? bodyWhite.copyWith(
+                                                      color: Colors.white)
+                                                  : bodyBlack.copyWith(
+                                                      color: Colors.black),
+                                            )),
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
                                 ),
                               ),
-                            );
-                          },
+                            )
+                          ],
                         ),
-                      ),
-                    )
-                  ],
-                ),
-              ],
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        CustomBackButton(text: 'Settings'),
+        )
       ],
     );
   }
